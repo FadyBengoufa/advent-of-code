5188ba5e05673d40485f695b2cca21402964db72
Fady Bengoufa <fady.bengoufa@gmail.com>
2024-12-17 08:41:24 +0100

    second day solution (wip)

---
+++ Day02/p1-sol.ts
@@ -0,0 +1,43 @@
+function totalSafeReports(input: string) {
+  let reports: number[][] = [];
+
+  // Prepare input
+  input.split(/\n/).map((report, idx) => {
+    reports.push(report.split(/\s+/).map((num) => parseInt(num)));
+  });
+
+  let count = 0;
+  for (let i = 0; i < reports.length; i++) {
+    if (isSafe(reports[i])) count++;
+  }
+  console.log("number of safe reports", count);
+  return count;
+}
+
+function isSafe(report: number[]) {
+  let isIncreasingCount = 0;
+  let isDecreasingCount = 0;
+  // Is report sequence increasing or decreasing
+  for (let i = 1; i < report.length; i++) {
+    const diff = Math.abs(report[i] - report[i - 1]);
+    if (report[i - 1] <= report[i] && diff >= 1 && diff <= 3) {
+      isIncreasingCount++;
+    } else if (report[i - 1] >= report[i] && diff >= 1 && diff <= 3) {
+      isDecreasingCount++;
+    }
+  }
+
+  if (
+    isIncreasingCount === report.length - 1 ||
+    isDecreasingCount === report.length - 1
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const file = Bun.file("input.txt");
+const input = await file.text();
+
+totalSafeReports(input);
