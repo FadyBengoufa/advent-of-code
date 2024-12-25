9f6b10e50b6b63124e0cf9551551e95a57841e7f
Fady Bengoufa <fady.bengoufa@gmail.com>
2024-12-25 07:30:53 +0100

    third day solution (wip)

---
+++ Day03/p1-sol.ts
@@ -0,0 +1,23 @@
+function sumOfMultiplyFunctions(input: string) {
+  const regexMultiply = /mul\((\d+),(\d+)\)/g;
+  const regexDont = /don't\(\)/g
+  const regexDo = /do\(\)/g
+
+  let result = 1;
+  let match;
+  let sum = 0
+
+
+  while ((match = regexMultiply.exec(input)) !== null) {
+    result *= parseInt(match[1]) * parseInt(match[2]);
+    sum += result
+    result = 1
+  }
+  console.log("sum", sum);
+  return input;
+}
+
+const file = Bun.file("input.txt");
+const input = await file.text();
+
+sumOfMultiplyFunctions(input);
